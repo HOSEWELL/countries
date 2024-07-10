14bf58faa248efc4dd0f0f5689f854c0507c6dbf
HOSEWELL <hosewellkaranja@gmail.com>
2024-07-10 10:28:06 +0300

    modifications

--- src/components/countrycontainer.jsx
+++ src/components/countrycontainer.jsx
@@ -1,43 +1,55 @@
 import React, { useState, useEffect } from "react";
 import Country from "./country";
 import NavBar from "./navbar";
+import axios from "axios";
 
 const CountryContainer = () => {
-
-    const [country, setCountry] = useState([])
+    const [country, setCountry] = useState([]);
+    const [search, setSearch] = useState("");
 
     useEffect(() => {
-        fetch('https://restcountries.com/v3.1/all')
+        axios
+            .get("https://restcountries.com/v3.1/all")
             .then((response) => {
-                response.json().then((resp) => {
-                    console.log(resp)
-                    setCountry(resp)
-                })
-            }).catch((err) => {
-                console.err(err);
+                setCountry(response.data);
             })
-    }, [])
+            .catch((error) => {
+                console.log(error);
+            });
+    }, []);
+
+    const handleSearch = (event) => {
+        setSearch(event.target.value);
+    };
+
+    const filteredCountries = country.filter((country) =>
+        country.name.common.toLowerCase().includes(search.toLowerCase())
+    );
 
     return (
         <>
             <NavBar />
             <br />
             <div className="container">
+                <input
+                style={{borderRadius:"10px",padding:"10px",margin:"10px"}}
+                    type="text"
+                    value={search}
+                    onChange={handleSearch}
+                    placeholder="Search for a country...."
+                />
                 <div className="row justify-content">
-                    {
-                        country.map((prod, index) => {
-                            return (
-                                <div className="col-md-4 mb-3" key={index}>
-                                    <Country country={prod} />
-                                </div>
-                            )
-                        })
-                    }
-
+                    {filteredCountries.map((prod, index) => {
+                        return (
+                            <div className="col-md-4 mb-3" key={index}>
+                                <Country country={prod} />
+                            </div>
+                        );
+                    })}
                 </div>
-
             </div>
         </>
-    )
-}
+    );
+};
+
 export default CountryContainer;
--- src/components/countryprofile.jsx
+++ src/components/countryprofile.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import NavBar from "./navbar";
 import { useLocation } from "react-router-dom";
+
 import { Link } from "react-router-dom";
 
 const CountryProfile = () => {
